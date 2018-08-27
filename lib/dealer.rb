a4884b259c5b8eb0f44d72a6160a97083d6a9299
Jason Numeroff <jnumeroff@hotmail.com>
2018-08-26 22:51:47 -0400

    Update dealer tests

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,17 +1,19 @@
 require_relative 'hand'
 
 class Dealer
+  attr_reader :hand
+
   def initialize
     @hand = Hand.new
   end
 
   def play_hand(shoe)
-    while @hand.value < 17
-      @hand.hit(shoe)
+    while hand.value < 17
+      hand.hit(shoe)
     end
   end
 
   def hit(shoe)
-    @hand.hit(shoe)
+    hand.hit(shoe)
   end
 end
